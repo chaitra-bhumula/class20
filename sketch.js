1a88145606e4a0e7ecfb90386cdb3fc431ec9e81
chaitra-bhumula <71412450+chaitra-bhumula@users.noreply.github.com>
2020-10-10 15:18:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite (500,200,60,80);
+  fixedRect.shapeColor="green";
+  movingRect = createSprite (280,90,60,80);
+  movingRect.shapeColor="green";
+}
+
+function draw() {
+  background("black");  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
+  && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+  && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
+  && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2){
+  movingRect.shapeColor="blue";
+  fixedRect.shapeColor="blue";
+}
+else{ movingRect.shapeColor="green";
+fixedRect.shapeColor="green";
+
+}
+
+
+  drawSprites();
+}
